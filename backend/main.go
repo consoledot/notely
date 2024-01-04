91542ebab305e43bd4bd62ca430ad639d2d53638
Abimbola Oladele <bimbooladele@outlook.com>
2024-01-04 10:03:47 +0100

    rewrote the server logic and db(mongo) connection)

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	db "github.com/consoledot/notely/database"
 	"github.com/consoledot/notely/notes"
 )
 
+func main() {
 
+	db.Connect()
+	defer db.Disconnect()
+	server := &http.Server{
+		Addr:    ":8181",
+		Handler: routes(),
+	}
 
+	fmt.Println("Server listening on port 8181")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalln("Server Crashed ", err)
+	}
 
-func main(){
-		server := http.NewServeMux()
+}
 
-		server.HandleFunc("/", notes.NotesController)
+func routes() http.Handler {
+	router := http.NewServeMux()
 
-		err := http.ListenAndServe(":8080", server)
-		if err != nil{
-			panic("Server failed to start")
-		}
+	router.HandleFunc("/", notes.NotesController)
+	return router
 }
