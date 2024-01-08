e52a842e84b74c1b4354111dc0b175b7b65928b4
Abimbola Oladele <bimbooladele@outlook.com>
2024-01-08 22:59:04 +0100

    log error and send as api also

--- backend/notes/controller.go
+++ backend/notes/controller.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	db "github.com/consoledot/notely/database"
 	httplib "github.com/consoledot/notely/utils"
-	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -21,12 +19,14 @@ func GetNotes(w http.ResponseWriter, r *http.Request) {
 	var result []Note
 	cusror, err := coll.Find(context.TODO(), bson.D{})
 	if err != nil {
-		log.Panic(err)
+		fmt.Println(err)
+		c.Response(false, nil, "Error getting notes", http.StatusNoContent)
 
 	}
 
 	if err = cusror.All(context.TODO(), &result); err != nil {
-		log.Panic(err)
+		fmt.Println(err)
+		c.Response(false, nil, "Error getting notes", http.StatusNoContent)
 	}
 
 	c.Response(true, result, "get all notes", http.StatusOK)
@@ -38,13 +38,14 @@ func CreateNewNotes(w http.ResponseWriter, r *http.Request) {
 	c := httplib.C{W: w, R: r}
 	var note Note
 	if err := json.NewDecoder(r.Body).Decode(&note); err != nil {
-
+		fmt.Println(err)
 		c.Response(false, nil, "error creating note", http.StatusBadRequest)
 		return
 	}
 
 	result, err := coll.InsertOne(context.TODO(), note)
 	if err != nil {
+		fmt.Println(err)
 		c.Response(false, nil, "error creating note", http.StatusBadRequest)
 		return
 	}
@@ -58,19 +59,21 @@ func DeleteNote(w http.ResponseWriter, r *http.Request) {
 	c := httplib.C{W: w, R: r}
 
 	// Get variables
-	vars := mux.Vars(r)
-	id := vars["id"]
+	id := c.GetParamsById(`id`)
 	objId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		fmt.Println("Id is not a primitive id: ", err)
+		fmt.Println(err)
+		c.Response(false, nil, "id is not a valid id", http.StatusBadRequest)
+		return
 	}
 
 	note := bson.D{{Key: "_id", Value: objId}}
 
 	result, err := coll.DeleteOne(context.TODO(), note)
 	if err != nil || result.DeletedCount <= 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Note not found"))
+
+		fmt.Println(err)
+		c.Response(false, nil, "note not found", http.StatusNotFound)
 		return
 	}
 	c.Response(true, nil, "note deleted", http.StatusOK)
@@ -86,8 +89,8 @@ func GetNote(w http.ResponseWriter, r *http.Request) {
 	id := c.GetParamsById(`id`)
 	objId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		fmt.Println("Id is not a primitive id: ", err)
-		w.Write([]byte("Not a valid ID"))
+		fmt.Println(err)
+		c.Response(false, nil, "id is not a valid id", http.StatusBadRequest)
 		return
 	}
 
@@ -96,9 +99,8 @@ func GetNote(w http.ResponseWriter, r *http.Request) {
 	var result Note
 	err = coll.FindOne(context.TODO(), note).Decode(&result)
 	if err != nil {
-		fmt.Println("Not found: ", err)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Note not found"))
+		fmt.Println(err)
+		c.Response(false, nil, "note not found", http.StatusNotFound)
 		return
 	}
 	c.Response(true, result, "note found", http.StatusFound)
